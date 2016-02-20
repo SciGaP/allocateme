f5ee2171f0ac4fc1d7db93bf0a7dead346ac4ed1
Laurynas <tamulev2@illinois.edu>
2016-02-20 12:09:49 -0600

    Added new dependancies

---
+++ allocate/src/main/java/org/apache/airavata/JsonRepresentation.java
@@ -0,0 +1,5 @@
+package org.apache.airavata;
+
+public class JsonRepresentation {
+
+}
--- allocate/src/main/java/org/apache/airavata/MongoTesting.java
+++ allocate/src/main/java/org/apache/airavata/MongoTesting.java
@@ -6,8 +6,9 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-
+ 
 
 /**
  * Created by samkreter on 2/20/16.
@@ -25,12 +26,16 @@ public class MongoTesting {
     public void printData(){
         FindIterable<Document> iterable = db.getCollection("restaurants").find();
         iterable.forEach(new Block<Document>() {
-            @Override
             public void apply(final Document document) {
-                JSONObject o =
                 System.out.println(document.get("address"));
             }
         });
+    }
+    public String readData(){
+        MongoClient mongoClient = new MongoClient();
+    	db = mongoClient.getDatabase("test");
+    	System.out.println(db.getName());
+    	return db.getName();
     }
 
 

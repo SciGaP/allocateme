a19112afaa9833dbd5a7ca18ac6d5bc3e7dd7096
Laurynas <tamulev2@illinois.edu>
2016-02-20 20:23:54 -0600

    JSON to DB implementation

--- allocate/src/main/java/org/apache/airavata/MongoWrapper.java
+++ allocate/src/main/java/org/apache/airavata/MongoWrapper.java
@@ -16,48 +16,54 @@ import org.json.simple.JSONValue;
  */
 public class MongoWrapper {
 
-    private MongoDatabase db;
-    private int results;
-    public MongoWrapper(){
-        MongoClient mongoClient = new MongoClient();
-        db = mongoClient.getDatabase("resource_allocation");
-    }
+	private MongoDatabase db;
+	private int results;
 
-    public boolean userInDB(JSONObject user){
-        FindIterable<Document> iterator = db.getCollection("user").find(
-                new Document("user.primaryEmail", user.get("primaryEmail"))
-        );
-        return iterator.iterator().hasNext();
-    }
+	public MongoWrapper() {
+		MongoClient mongoClient = new MongoClient();
+		db = mongoClient.getDatabase("resource_allocation");
+	}
 
-    public void createUser(JSONObject user) {
-        BasicDBObject documentUser = new BasicDBObject();
-        db.getCollection("user").insertOne(
-                new Document("user",
-                        new Document()
-                                .append("name", user.get("name"))
-                                .append("primaryEmail", user.get("primaryEmail")))
-        );
-    }
+	public boolean userInDB(JSONObject user) {
+		FindIterable<Document> iterator = db.getCollection("user")
+				.find(new Document("user.primaryEmail", user.get("primaryEmail")));
+		return iterator.iterator().hasNext();
+	}
 
-    /**
+	public void createUser(JSONObject user) {
+		BasicDBObject documentUser = new BasicDBObject();
+		db.getCollection("user").insertOne(new Document("user",
+				new Document().append("name", user.get("name")).append("primaryEmail", user.get("primaryEmail"))));
+	}
+
+	/**
      * Take a string formatted representation of the data and add it to the database
      */
-    public void addJSONtoDB(String obj, String collection){
-        DBObject dbObject = (DBObject) JSON.parse(obj);
-//        db.getCollection(collection).insertOne();
-    }
-    
-    /**
-     * @param primaryEmail The user's primary email address to query the database.
-     * Returns a JSONObject containing all of the user's data
-     */
-    public JSONObject getUser(String primaryEmail){
-        FindIterable<Document> iterator = db.getCollection("user").find(
-                new Document("user.primaryEmail", primaryEmail)
-        );
-        Document d = iterator.iterator().next();
-        return (JSONObject) JSONValue.parse(d.toJson());
-    }
+	
+    public void addJSONtoDB(JSONObject user){
+//    	db.getCollection("user").insertOne(Document.parse(user.toJSONString()));
+    	Document query1 = new Document();
+    	Document query2 = new Document();
+    	
+    	query1.append("user",new Document().append("primaryEmail", user.get("primaryEmail")));
+    	
+    	query2.append("$set", new Document().append("publications", (JSONArray) user.get("publications"))
+											.append("institution", new Document("verified",user.get("verified")))
+											.append("tier",user.get("tier"))
+											.append("funding", user.get("funding")));
+    	db.getCollection("user").updateOne(query1, query2);
+       }
+
+	/**
+	 * @param primaryEmail
+	 *            The user's primary email address to query the database.
+	 *            Returns a JSONObject containing all of the user's data
+	 */
+	public JSONObject getUser(String primaryEmail) {
+		FindIterable<Document> iterator = db.getCollection("user")
+				.find(new Document("user.primaryEmail", primaryEmail));
+		Document d = iterator.iterator().next();
+		return (JSONObject) JSONValue.parse(d.toJson());
+	}
 
 }
